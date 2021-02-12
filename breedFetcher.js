365d20a35d85b8310f9ac9b68546ea16ec9408de
Samira Bani Hassan <samira.banihassan@gmail.com>
2021-02-12 13:59:57 +0000

    breedFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,16 @@
+const request = require("request");
+
+let fetchBreedDescription = function(breed, cb) {
+  const resource = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(resource, (error, response, body) => {
+    if (error) {
+      cb(error, null);
+    }
+    let result = JSON.parse(body);
+    if (result.length === 0) {
+      cb("Not a Valid Breed", null);
+    } else cb(null, result[0].description);
+  });
+};
+
+// module.exports = { fetchBreedDescription };
